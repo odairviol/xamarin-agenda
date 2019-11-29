6004b53f2e2c657ade777668ddfdb8d40d574f30
odair.viol@gmail.com <pos@Mac-mini-de-IFSP.local>
2019-11-29 20:48:54 -0300

    Exemplo xamarin com SQLite

---
+++ Agenda/Agenda/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Agenda
+{
+    public partial class App : Application
+    {
+        private static PessoaItemDatabase dataBase;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        internal static PessoaItemDatabase Database
+        {
+            get
+            {
+                if(dataBase == null)
+                {
+                    dataBase = new PessoaItemDatabase(DependencyService.Get<IFileHelper>().GetLocalFilePath("PessoaSQLite.db3"));
+                }
+                return dataBase;
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Agenda/Agenda/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SQLite;
+namespace Agenda
+{
+    public class Pessoa
+    {
+        [PrimaryKey, AutoIncrement]
+        public uint ID { get; set; }
+        public String Nome { get; set; }
+        public String Telefone { get; set; }
+        public String Email { get; set; }
+    }
+}
